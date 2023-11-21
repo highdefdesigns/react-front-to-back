50f4e1d47e3166e79befb34272e1c5ab3f0fe74f
Justin Lee Edwards <justin@highdefdesigns.com>
2023-11-20 22:35:53 -0600

    react initial setup

--- feedback-app/src/App.jsx
+++ feedback-app/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  const title = 'Blog Post';
+  const body = 'this is my blog post';
+  const comments = [
+    { id: 1, text: 'comment 1' },
+    { id: 2, text: 'comment 2' },
+    { id: 3, text: 'comment 3' },
+  ];
+
+  const loading = false;
+  const showComments = true;
+
+  if (loading) return <h1>Loading...</h1>;
+
+  const commentBlock = (
+    <div className="comments">
+      <h3>Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <h1>{title.toUpperCase()}</h1>;<p>{body}</p>
+      {showComments && commentBlock}
+    </div>
+  );
 }
 
-export default App
+export default App;
