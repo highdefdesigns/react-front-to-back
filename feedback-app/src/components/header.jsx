c4ca7791c7e833abe4875deefdedb3166f6a6855
Justin Lee Edwards <justin@highdefdesigns.com>
2023-11-28 21:52:48 -0600

    adding styles

--- feedback-app/src/components/header.jsx
+++ feedback-app/src/components/header.jsx
@@ -1,12 +1,22 @@
-function Header({ text }) {
+function Header({ text, bgColor, color }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color,
+  };
+
   return (
-    <div className="container">
-      <h1>{text}</h1>
-    </div>
+    <header style={headerStyles}>
+      <div className="container">
+        <h2>{text}</h2>
+      </div>
+    </header>
   );
 }
 
 Header.defaultProps = {
   text: 'Feedback UI',
+  bgColor: 'rgba(0,0,0,0.4)',
+  color: 'red',
 };
+
 export default Header;
