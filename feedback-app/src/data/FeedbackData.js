0e4567739a157d6f1fa36c229fcea576e0a47760
Justin Lee Edwards <justin@highdefdesigns.com>
2023-11-28 23:36:07 -0600

    global state on feedback items

---
+++ feedback-app/src/data/FeedbackData.js
@@ -0,0 +1,24 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis sit voluptatibus maxime quasi? Autem corrupti facere eaque dolore vel natus.',
+  },
+  {
+    id: 2,
+    rating: 5,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores ab laboriosam tempore ipsum, quo repudiandae nihil magni! Harum iusto ad sed pariatur, adipisci molestias debitis.',
+  },
+  {
+    id: 3,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis sit voluptatibus maxime quasi? Autem corrupti facere eaque dolore vel natus.',
+  },
+  {
+    id: 4,
+    rating: 6,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis sit voluptatibus maxime quasi? Autem corrupti facere eaque dolore vel natus.',
+  },
+];
+
+export default FeedbackData;
